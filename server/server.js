a42738a88612057912d76f6b95676e02294e8c1e
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-11-18 12:19:20 +0100

    init

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+// const userRoute = require("./routes/user");
+// const authRoute = require("./routes/auth");
+// const productRoute = require("./routes/product");
+// const cartRoute = require("./routes/cart");
+// const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
 
+//setup mongoose
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+// Routes
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/products", productRoute);
+// app.use("/api/carts", cartRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
